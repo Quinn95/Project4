75407b24aec3921c6105c8508f17f684dfba5287
UT-AliZiyaan <alimomin@utexas.edu>
2016-10-20 13:23:55 -0500

    some more debugging and now old MyCritter1 is now called ManualCritter

--- critters1/src/assignment4/MyCritter1.java
+++ critters1/src/assignment4/MyCritter1.java
@@ -1,31 +1,12 @@
 package assignment4;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class MyCritter1 extends Critter.TestCritter {
 
 	@Override
 	public void doTimeStep() {
-		//System.out.println(this.getEnergy());
-		System.out.println("place critter (" + this.getX_coord() + "," + this.getY_coord() + ") at: ");
-		Scanner s = new Scanner(System.in);
-		int input = s.nextInt();
-		int input2 = s.nextInt();
-		this.setX_coord(input);
-		this.setY_coord(input2);
-
-		System.out.println("Do you want to have a baby: ");
-		String babyQ = s.next();
-		//int babyDirection;
-		if(babyQ.equals("y")){
-			System.out.println("enter direction: ");
-			int babyDirection = s.nextInt();
-			MyCritter1 myBaby = new MyCritter1();
-			reproduce(myBaby, babyDirection);
-		}
-		System.out.println("MyCritter 1 energy: " + this.getEnergy());
-		//walk(0);
+		walk(0);
 	}
 
 	@Override
